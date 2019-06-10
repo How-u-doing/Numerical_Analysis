14156bedc46a7710b60d56ea43f005711fdd3952
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-06-10 11:26:44 +0800

    Add files via upload

--- Chapter01_MatrixAnalysis/givens_QR.m
+++ Chapter01_MatrixAnalysis/givens_QR.m
@@ -1,6 +1,6 @@
-% Givens QR Method
+% QR decomposition via Givens Rotation 
 % 10170437 Mark Taylor
-function [Q,R]=G_QR(A)
+function [Q,R]=givens_QR(A)
 [m,n] = size(A);
 if m < n 
     error('The number of rows must be no less than the number of columns!')
@@ -13,9 +13,10 @@ Q=eye(m);
 for j=1:n
     for i=j+1:m
         if R(i,j)~=0
-            T=Givens(R(j,j),R(i,j),j,i,m);
-            R=T*R;     % m==n: R=T(n-1,m)T(n-2,m)...T(1,m)...T(1,2)A
-            Q=T*Q;     % m==n: Q=T(1,2)...T(1,m)...T(n-2,m)T(n-1,m)
+            G=givens_cs(R(j,j),R(i,j));
+            R([j,i],:)=G*R([j,i],:);
+            Q([j,i],:)=G*Q([j,i],:);
+            
         else 
             continue;
         end
