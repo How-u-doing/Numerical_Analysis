6e9f532e66e6bab046964623f69a0bdee7e6c869
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-06-26 19:37:54 +0800

    Add files via upload

--- Chapter01_MatrixAnalysis/H_QR.m
+++ Chapter01_MatrixAnalysis/H_QR.m
@@ -9,8 +9,8 @@ end
 R=A;
 Q=eye(n);
 for j=1:n-1                             %     _             _
-    H=eye(n);                           % H= |  Ij-1     0   | ¡ú(j-1) rows
-    H(j:n,j:n)=Householder(R(j:n,j));   %    |_  0      Hj  _| ¡ú(n-(j-1)) rows
+    H=eye(n);                           % H= |  Ij-1     0   | ->(j-1) rows
+    H(j:n,j:n)=Householder(R(j:n,j));   %    |_  0      Hj  _| ->(n-(j-1)) rows
     
     R=H*R;                              % R=H(n-1)...H(2)H(1)*A                                       
     Q=Q*H;                              % Q=H(1)H(2)...H(n-1)
