dc7ab022fb7fddc3dc77c661a7a0ce99fed83881
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-06-27 14:51:18 +0800

    Add files via upload

--- Chapter02_DirectMethod/Cholesky.m
+++ Chapter02_DirectMethod/Cholesky.m
@@ -1,18 +1,17 @@
 % Cholesky Factorization, the decomposition of a symmetric positive definite  
 % matrix into the product of a lower triangular matrix and its transpose.
 % 10170437 Mark Taylor
-function [x,L]= Cholesky(A,b)
+function [L,x]= Cholesky(A,b)
     
 [m,n]=size(A);
-if m ~= n
-    error('A must be symmetric positive definite!')
-elseif isequal(A,A')==false || allPositive(eig(A))==false
+if m ~= n || isequal(A,A.')==false
     error('A must be symmetric positive definite!')
 end
 
 L=zeros(n);
-% Perform Cholesky's method with no need to pivoting
-% since A is symmetric positive definite.
+% Perform Cholesky's method with no need to pivoting, since A is 
+% symmetric positive definite, which determines its stability of
+% eliminations in natural order.
 for j=1:n
     L(j,j)=sqrt(A(j,j)-L(j,1:j-1)*L(j,1:j-1).');
     
@@ -29,6 +28,10 @@ if nargin==2
 % A=L*L.', L*y=b, y=L.'*x, => A*x=b
 y=solveTril(L,b);
 x=solveTriu(L.',y);
+
+if nargout<=1
+    L=x;
+end
 
 end
 
