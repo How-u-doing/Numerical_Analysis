52da3ca852337c0463379b59bbee79e4a9d7852c
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-09-11 11:28:55 +0800

    stability test
    High degree interpolating polynomial may cause unstability

---
+++ Chapter06_Interpolation/polynomial interpolation/stability_test.m
@@ -0,0 +1,25 @@
+% Stability test of interpolating polynomials of different degrees
+
+t=-5:0.1:5;
+y=1./(1.+t.^2);
+syms x;
+% construct 6 sample points
+x1=(-5:2:5).';
+y1=1./(1.+x1.^2);
+P5=simplify(Newton([x1,y1]));
+P5=vpa(P5,7);
+Y1=subs(P5,x,t);
+
+% construct 11 sample points
+x2=(-5:5).';
+y2=1./(1.+x2.^2);
+P10=simplify(Newton([x2,y2]));
+P10=vpa(P10,7);
+Y2=subs(P10,x,t);
+
+% draw an image that consists of P5(x),P10(x) & f(x)=1/(1+x^2)
+figure('Name','stability test','NumberTitle','off');
+plot(t,Y1,'r',t,Y2,'b',t,y,'k');
+legend('P5(x)','P10(x)','f(x)');
+title('Unstability of high degree interpolating polynomial');
+
