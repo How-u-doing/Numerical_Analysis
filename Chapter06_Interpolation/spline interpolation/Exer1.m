abe60b27d820f0e9140ccf356508cf0e19505b9f
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-09-23 21:57:28 +0800

    add figure

--- Chapter06_Interpolation/spline interpolation/Exer1.m
+++ Chapter06_Interpolation/spline interpolation/Exer1.m
@@ -4,8 +4,17 @@ x=-1:2;
 y=[4 1 6 26];
 dya=-6;dyb=15;
 s=CShw(x,y,dya,dyb);
+syms t;
+X=zeros(3,11);Y=X;
 for i=1:3
-    fprintf('%d<=t<=%d:\n',i-2,i-1);
+    fprintf('%d<=t<=%d:\n',i-2,i-1);    
     S=s(i)
+    X(i,:)=i-2:.1:i-1;
+    Y(i,:)=subs(S,t,X(i,:));
 end
+X=[X(1,:),X(2,:),X(3,:)];
+Y=[Y(1,:),Y(2,:),Y(3,:)];
+plot(x,y,'o',X,Y,'r--')
+legend('Sample points','Cubic Spline')
+
 
