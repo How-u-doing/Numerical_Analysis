30763f38c04ac9616950b62f5cf2511e18b71c73
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-11-11 14:50:49 +0800

    Find a sily mistake

--- Chapter07_Approximation/Fourier_transform/FFT.cpp
+++ Chapter07_Approximation/Fourier_transform/FFT.cpp
@@ -9,19 +9,17 @@ typedef std::complex<double> cp;
 
 int main()
 {
-	cp* FFT(cp* A, const int N);
+	cp* FFT(cp * A, const int N);
 	const int N = 4;
 	cp* A = new cp[N];
 	for (int i = 1; i < N; ++i)
-		A[i] = cp(i,0);	
+		A[i] = cp(i, 0);
 	cp* x = FFT(A, N);
 
 	std::cout << "After Fourier transform: \n";
 	for (int i = 0; i < N; ++i) {
 		std::cout << x[i] << '\n';
 	}
-
-	std::cout << x[1]+x[2] << '\n';
 	delete[] x;
 	delete[] A;
 	return 0;
@@ -34,15 +32,15 @@ cp* FFT(cp* A, const int N)
 	int p = int(log2(N));
 	if (p != log2(N)) {
 		std::cerr << "log2(N) must be an integer!\n";
-		exit(1);	
+		exit(1);
 	}
 
 	cp* A0 = new cp[N];
 	// duplicate A to evade to change it
 	for (int i = 0; i < N; ++i)
-		* (A0 + i) = *(A + i);
-	
-	cp* w = new cp[N / 2 - 1];
+		*(A0 + i) = *(A + i);
+
+	cp* w = new cp[N / 2];
 	for (int j = 0; j <= N / 2 - 1; ++j)
 		w[j] = cp(cos(-2 * pi * j / N), sin(-2 * pi * j / N));
 
@@ -64,17 +62,16 @@ cp* FFT(cp* A, const int N)
 						A1[k * int(pow(2, q - 1)) + j + int(pow(2, p - 1))]) * w[k * int(pow(2, q - 1))];
 				}
 	}
+
 	
-	// It's very weird that it throws an exception (Stack.exe has triggered a breakpoint.)
-	// when I attempt to release w. ??
 	delete[] w;
 
 	if (p % 2 == 0) {
 		delete[] A1;
 		return A0;
-	}		
+	}
 	else {
 		delete[] A0;
-		return A1;			
+		return A1;
 	}
 }
