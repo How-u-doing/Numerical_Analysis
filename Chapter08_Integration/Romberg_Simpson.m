6273a3ca3bf2107e5101dab9f791d8c6af7dcd3b
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-11-26 14:47:04 +0800

    fix some issues and add Romberg_Simpson

--- Chapter08_Integration/Romberg_Simpson.m
+++ Chapter08_Integration/Romberg_Simpson.m
@@ -1,5 +1,5 @@
-% Romberg Integration (iterative method)
-function [T,k]=Romberg(f,a,b,maxIt,tol)
+% Romberg Simpson Integration (iterative method)
+function [S,k]=Romberg_Simpson(f,a,b,maxIt,tol)
 % set default args
 if nargin<5
     tol=1e-6;
@@ -11,29 +11,20 @@ if nargin<5
     end
 end
 
-h=(b-a);
 % R1, R2 are used to store the most recent rows
 R1=zeros(1,maxIt+1);
-R1(1)=h/2*(f(a)+f(b));
+R1(1)=CompositeInt(f,a,b,1,"Simpson");
 for k=1:maxIt
-    n=2^(k-1);
-    h=(b-a)/n; % h is the even interval of R1
-    H=0;
-    for i=0:n-1
-        % when n is large, say 2^25, it's computation expensive 
-        H=H+f(a+(i+1/2)*h);
-    end
-    H=h*H;
     
     R2=zeros(1,maxIt+1);
-    R2(1)=1/2*(R1(1)+H);
+    R2(1)=CompositeInt(f,a,b,2^k,"Simpson");
     for j=2:k+1
         R2(j)=1/(4^(j-1)-1)*(4^(j-1)*R2(j-1)-R1(j-1));
     end
     
     % tell when to stop
     if abs(R2(k+1)-R1(k))<tol
-        T=R2(k+1);
+        S=R2(k+1);
         return
     end
     
@@ -42,6 +33,6 @@ for k=1:maxIt
 end
 % Maximum number of iterations exceeded (the procedure was successful).
 fprintf('Maximum no. of iterations exceeded, the results of last iteration are as follows:\n')
-T=R2(maxIt+1);
+S=R2(maxIt+1);
 end
 
--- Chapter08_Integration/Romberg_test.m
+++ Chapter08_Integration/Romberg_test.m
@@ -2,4 +2,11 @@
 f=@(x) sin(x);
 a=0;
 b=pi;
+format long
+% for maxIt=1:6
+%     fprintf('maxIt=%d\n',maxIt)
+%     [T,k]=Romberg(f,a,b,maxIt)
+% end
 [T,k]=Romberg(f,a,b)
+[S,k]=Romberg_Simpson(f,a,b)
+
