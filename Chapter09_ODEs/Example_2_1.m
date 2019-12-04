f20878a6c4e3a9e54014a679b9a205b12fb4e739
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-12-04 17:13:07 +0800

    added convergence order

--- Chapter09_ODEs/Example_2_1.m
+++ Chapter09_ODEs/Example_2_1.m
@@ -18,17 +18,18 @@ plot(t,y,'g',t,y1,'b--',t,y2,'r--',t,y3,'c--',t,y4,'m--',t,y5,'k--')
 legend('Original Function','Forward Euler','Backward Euler','Modified Euler','Midpoint','Runge-Kutta')
 
 % compare Modified Euler(explicit Trapezoidal), Trapezoidal, Midpoint(Runge-Kutta method of order 2)
-% zoom in you'll find fitting effect: Midpoint > Trapezoidal > Modified Euler
+% In this case, you'll find fitting effect: Midpoint > Trapezoidal > Modified Euler,
+% despite the fact that their convergence orders indeed are the same (p = 2).
 figure(2)
 [t, y6]=Trapezoidal(f,a,b,ya,n);
-plot(t,y,'g',t,y3,'b--',t,y4,'r--',t,y6,'m--')
+plot(t,y,'g',t,y3,'b--',t,y4,'r--',t,y6,'c--')
 legend('Original Function','Modified Euler','Midpoint','Trapezoidal')
 
 
 % error = Rh = O(h^n), log(Rh)=n*log(h), slope k=n...
 % which represents its convergence order. 
-% So we need to let log(h) be the x axis, log(err) be... 
-% the y axis and see the slopes of these methods look like.
+% So we need to let log(h) be the x axis, log(err) be the... 
+% y axis and see how the slopes of these methods look like.
 n=10;
 N=8;    % no. of points of log(h)
 h=zeros(1,N);
@@ -61,4 +62,22 @@ legend('Forward Euler','Backward Euler','Modified Euler','Midpoint','Runge-Kutta
 xlabel('log(h)')
 ylabel('log(err)')
 title('Error Comparison')
+
+% get convergence orders of these methods (i.e. their slopes)
+P1=polyfit(logh,loge(1,:),1); % use linear polynomial to fit, i.e. y = k*x + b, so P1 = [k b] 
+p_Forward_Euler = P1(1)       % its convergence order should amount to its slope k
+                              % it may not be an integer, but it is pretty
+                              % much close to one, say 0.9777 or whatever
+
+P2=polyfit(logh,loge(2,:),1);
+p_Backward_Euler = P2(1)
+
+P1=polyfit(logh,loge(3,:),1);
+p_Modified_Euler = P1(1)
+
+P1=polyfit(logh,loge(4,:),1);
+p_Midpoint = P1(1)
+
+P1=polyfit(logh,loge(5,:),1);
+p_Runge_Kutta = P1(1)
 
