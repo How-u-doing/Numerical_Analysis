3fd637626b8564236d0b9630043b85bce9023653
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-12-06 12:26:50 +0800

    move to old

--- Chapter06_Interpolation/polynomial interpolation/old/LagrangeAndNewton_test.m
+++ Chapter06_Interpolation/polynomial interpolation/old/LagrangeAndNewton_test.m
@@ -16,7 +16,7 @@ fprintf('which indicates Lagrange & Newton interpolating polynomials are the sam
 fprintf('\n\nfunction LagrangeSolve() & LagrangePS() test:\n');
 % f(x)=x^4-3x+1
 table2=[0.1,0.70010;0.2,0.40160;0.3,0.10810;0.4,-0.17440]
-% If you run these two functions in command window respectively£¬
+% If you run these two functions in command window respectivelyÂ£Â¬
 % you'll find the latter runs apparently faster than the former one.
 % Reason's simple: latter doesn't need to call syms family, which
 % saves a lot of time.
