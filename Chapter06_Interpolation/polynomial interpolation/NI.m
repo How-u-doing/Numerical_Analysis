c77dbc6cb97646f129f3df7f29f2e7eb6949724c
Mark Taylor <46680412+How-u-doing@users.noreply.github.com>
2019-12-11 19:45:54 +0800

    A little optimization

--- Chapter06_Interpolation/polynomial interpolation/NI.m
+++ Chapter06_Interpolation/polynomial interpolation/NI.m
@@ -1,5 +1,4 @@
 % Newton Interpolation
-% 10170437 Mark Taylor
 function s = NI(x,y,xq)
 % x,y: vector x & y are sample points and their corresponding function values, respectively
 % xq: query points, specified as a vector, to be evaluated
@@ -8,15 +7,13 @@ n=length(x);
 A=NaN(n);   % coefficient matrix of divided differences
 A(:,1)=y;
 s=y(1);
+t=1;
 for j=2:n
     for i=j:n
         A(i,j)=(A(i,j-1)-A(i-1,j-1))/(x(i)-x(i-j+1));
     end
-    t=1;
-    for k=1:j-1
-        t=t.*(xq-x(k));
-    end
-    s=s+A(j,j).*t;
+    t=t.*(xq-x(j-1));
+    s=s+A(j,j)*t;
 end
 end
 
