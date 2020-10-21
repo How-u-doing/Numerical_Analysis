f62f5f7e7d474f1b3fd8bcb3bad9299c90fb2d92
Mark Taylor <2942224119@qq.com>
2020-10-21 11:07:57 +0800

    Use ceil to handle floating-point numbers

--- Chapter10_BVPforODEs/PLRR_intpol.m
+++ Chapter10_BVPforODEs/PLRR_intpol.m
@@ -7,26 +7,28 @@ xx=linspace(a,b,N+1);
 h=(b-a)/N;
 
 % a lazzy solution
-for i=1:N
-    for j=1:m
-        if xq(j)>=xx(i) && xq(j)<=xx(i+1)
-            if i==1
-                u(j)=uc(1)*(xq(j)-xx(1))/h;
-                continue
-            end
-            u(j)=uc(i-1)*(-(xq(j)-xx(i+1))/h)+uc(i)*(xq(j)-xx(i))/h;
-        end
-    end
-end
+% for i=1:N
+%     for j=1:m
+%         if xq(j)>=xx(i) && xq(j)<=xx(i+1)
+%             if i==1
+%                 u(j)=uc(1)*(xq(j)-xx(1))/h;
+%                 continue
+%             end
+%             u(j)=uc(i-1)*(-(xq(j)-xx(i+1))/h)+uc(i)*(xq(j)-xx(i))/h;
+%         end
+%     end
+% end
 
 % When xq is an *ordered* further division based on xx,
 % for example, xq = linspace(a,b,10*N+1), then an eager
 % version can be developed like this:
-%{
+% {
 k = (m-1)/N;
 for i=1:N
-    k1 = 1+k*(i-1);
-    k2 = 1+k*i;
+    % Note that, we are better off using floor, since floor(k * N)
+    % might NOT be equal to m-1, in turn, xq(m) remains what it is.
+    k1 = 1 + ceil(k*(i-1)); % Get upper bounds, at most 1 element of
+    k2 = 1 + ceil(k*i);     % xq(k1:k2) NOT in [xx(i), xx(i+1)].
     for j=k1:k2
         if xq(j)>=xx(i) && xq(j)<=xx(i+1)
             if i==1
@@ -38,5 +40,15 @@ for i=1:N
     end
 end
 %}
+
+% ************************FOOTNOTE***************************
+% Say, m=101, N=3, floor(k*N)=100?? Luckily, this is true in 
+% MATLAB (C, C++, Python, etc.) due to the fact that: 
+% >> k=100/3; % k = 33.333333333333336
+% >> k*3==100
+% ans = (logical) 1
+% That's how double-precision numbers store and operate.
+% Notice that floor(33.3333*3) = 99, NOT 100.
+% ************************FOOTNOTE***************************
 end
 
--- Chapter10_BVPforODEs/PQRR_intpol.m
+++ Chapter10_BVPforODEs/PQRR_intpol.m
@@ -11,29 +11,31 @@ phi2 = @(k)4*k.*(1-k);
 phi3 = @(k)k.*(2*k-1);
 
 % a lazzy solution
-for i=1:N
-    for j=1:m
-        if xq(j)>=xx(i) && xq(j)<=xx(i+1)
-            if i==1
-                u(j)=uc(1)*phi2((xq(j)-xx(1))/h)+...
-                        uc(2)*phi3((xq(j)-xx(1))/h);
-            else
-                u(j)=uc(2*i-1)*phi2((xq(j)-xx(i))/h)+...
-                        uc(2*i)*phi3((xq(j)-xx(i))/h)+...
-                        uc(2*i-2)*phi1((xq(j)-xx(i))/h);
-            end
-        end
-    end
-end
+% for i=1:N
+%     for j=1:m
+%         if xq(j)>=xx(i) && xq(j)<=xx(i+1)
+%             if i==1
+%                 u(j)=uc(1)*phi2((xq(j)-xx(1))/h)+...
+%                         uc(2)*phi3((xq(j)-xx(1))/h);
+%             else
+%                 u(j)=uc(2*i-1)*phi2((xq(j)-xx(i))/h)+...
+%                         uc(2*i)*phi3((xq(j)-xx(i))/h)+...
+%                         uc(2*i-2)*phi1((xq(j)-xx(i))/h);
+%             end
+%         end
+%     end
+% end
 
 % When xq is an *ordered* further division based on xx,
 % for example, xq = linspace(a,b,10*N+1), then an eager
 % version can be developed like this:
-%{
+% {
 k = (m-1)/N;
 for i=1:N
-    k1 = 1+k*(i-1);
-    k2 = 1+k*i;
+    % Note that, we are better off using floor, since floor(k * N)
+    % might NOT be equal to m-1, in turn, xq(m) remains what it is.
+    k1 = 1 + ceil(k*(i-1)); % Get upper bounds, at most 1 element of
+    k2 = 1 + ceil(k*i);     % xq(k1:k2) NOT in [xx(i), xx(i+1)].
     for j=k1:k2
         if xq(j)>=xx(i) && xq(j)<=xx(i+1)
             if i==1
@@ -48,5 +50,15 @@ for i=1:N
     end
 end
 %}
+
+% ************************FOOTNOTE***************************
+% Say, m=101, N=3, floor(k*N)=100?? Luckily, this is true in 
+% MATLAB (C, C++, Python, etc.) due to the fact that: 
+% >> k=100/3; % k = 33.333333333333336
+% >> k*3==100
+% ans = (logical) 1
+% That's how double-precision numbers store and operate.
+% Notice that floor(33.3333*3) = 99, NOT 100.
+% ************************FOOTNOTE***************************
 end
 
