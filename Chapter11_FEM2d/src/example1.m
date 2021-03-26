54cf39943fc73f640ac638118b45cb144d6b9a9e
Mark Taylor <17marktaylor@gmail.com>
2021-03-26 17:46:39 +0800

    Tweak for accuracy

--- Chapter11_FEM2d/src/example1.m
+++ Chapter11_FEM2d/src/example1.m
@@ -2,30 +2,35 @@
 % $-\Delta{u} = 2\pi^2 cos(\pi x)\,cos(\pi y)$, in $\Omega$
 % $\nabla{u}\cdot\boldsymbol{n}=\pi\,sin(\pi x)\,cos(\pi y)$, on $\Gamma$
 % $u=cos(\pi x)\,cos(\pi y)$, on $\partial{\Omega}\ {\backslash}\ {\Gamma}$
-% where $\Omega=\{(x,y)\ | \ -\frac{1}{2}\leq x \leq 1,\ -1 \leq y \leq 1 \}$,
+% where $\Omega=\{(x,y)\ | \ -\frac{1}{2} < x < 1,\ -1 < y < 1 \}$,
 % $\Gamma = \{(x,y)\ | \ x=-\frac{1}{2},\ -1 \leq y \leq 1 \}$
 
 N=6;
 L_inf_err=zeros(N,1);
 L2_err=zeros(N,1);
+H1_semi_err=zeros(N,1);
 H1_err=zeros(N,1);
 dofs=zeros(N,1);
 n=4;
 for i=1:N
-    [dofs(i),L_inf_err(i),L2_err(i),H1_err(i)]=run_example1(n);
+    [dofs(i),L_inf_err(i),L2_err(i),H1_semi_err(i),H1_err(i)]=run_example1(n);
     n=n*2;
 end
-L_inf_convergence_order=zeros(N-1,1);
-L2_convergence_order=zeros(N-1,1);
-H1_convergence_order=zeros(N-1,1);
+L_inf_order=zeros(N-1,1);
+L2_order=zeros(N-1,1);
+H1_semi_order=zeros(N-1,1);
+H1_order=zeros(N-1,1);
 for i=1:N-1
-    L_inf_convergence_order(i)=log2(L_inf_err(i)/L_inf_err(i+1));
-    L2_convergence_order(i)=log2(L2_err(i)/L2_err(i+1));
-    H1_convergence_order(i)=log2(H1_err(i)/H1_err(i+1));
+    L_inf_order(i)=log2(L_inf_err(i)/L_inf_err(i+1));
+    L2_order(i)=log2(L2_err(i)/L2_err(i+1));
+    H1_semi_order(i)=log2(H1_semi_err(i)/H1_semi_err(i+1));
+    H1_order(i)=log2(H1_err(i)/H1_err(i+1));
 end
-L_inf_convergence_order
-L2_convergence_order
-H1_convergence_order
+dofs
+disp('L_inf_err   L2_err   H1_semi_err   H1_err')
+disp([L_inf_err,L2_err,H1_semi_err,H1_err])
+disp('L_inf_order   L2_order   H1_semi_order   H1_order')
+disp([L_inf_order,L2_order,H1_semi_order,H1_order])
 
 % plot
 figure(3)
@@ -34,17 +39,20 @@ figure(3)
 loglog(dofs,L_inf_err,'-^', ...
     dofs,L2_err,'-d', ...
     dofs,exp(-1*log(dofs)+0.8),'--', ...
-    dofs,H1_err,'-p', ...
+    dofs,H1_semi_err,'-p', ... dofs,H1_err,'-h', ...
     dofs,exp(-1/2*log(dofs)+1.9),'--');
 xlabel('dofs [log]'); xlim([10 3*10^4]);
 ylabel('error [log]');
-legend('maximum norm','L^2 norm','rate 2','H^1 semi-norm', ...
+legend('maximum norm','L^2 norm','rate 2','H^1 semi-norm', ... 'H^1 norm',
     'rate 1','Location','SouthWest');
-% title('Convergence Orders');
+% title('Convergence Rates');
+% Since L^2 norm errors are rather small when compared to H^1 semi-norm,
+% H^1 semi-norm errors are almost equal to H^1 semi norm, so in the plot
+% H^1 semi-norm and H^1 norm almost coincide.
 
 %=================================================================
 
-function [dofs,L_inf_err,L2_err,H1_err] = run_example1(n)
+function [dofs,L_inf_err,L2_err,H1_semi_err,H1_err] = run_example1(n)
 x=[-1/2 1]; y=[-1 1];
 % n_x=50; n_y=50;
 n_x=n; n_y=n;
@@ -67,7 +75,7 @@ u = A\phi;
 
 u_r = @(x, y) cos(pi*x).*cos(pi*y);
 grad_u_r = @(x, y) [-pi*sin(pi*x).*cos(pi*y),-pi*cos(pi*x).*sin(pi*y)];
-[L_inf_err,L2_err,H1_err] = errorEstimates(p,t,u,u_r,grad_u_r);
+[L_inf_err,L2_err,H1_semi_err,H1_err] = errorEstimates(p,t,u,u_r,grad_u_r);
 
 % t is specified as a 4-by-Nt matrix in pdemesh, although we don't
 % use the 4th row in a 2D mesh. To be compatible with 3D meshes?
